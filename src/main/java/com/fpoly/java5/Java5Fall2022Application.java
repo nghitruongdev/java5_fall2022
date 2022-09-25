332858313f7a8d465ce7af6855cc609b61a4185a
Nghi Truong <89492293+vntruongdev@users.noreply.github.com>
2022-09-25 19:54:46 +0700

    Initial commit

---
+++ src/main/java/com/fpoly/java5/Java5Fall2022Application.java
@@ -0,0 +1,13 @@
+package com.fpoly.java5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java5Fall2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java5Fall2022Application.class, args);
+	}
+
+}
---
+++ src/main/webapp/views/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Java5-Fall2022</title>
+</head>
+<body>
+Hello World
+</body>
+</html>
