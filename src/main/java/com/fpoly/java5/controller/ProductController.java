0677cc7c6fb5fec5a8669c14caaa4fa0f64a38b0
nghitruong <89492293+vntruongdev@users.noreply.github.com>
2022-10-07 21:52:58 +0700

    Haoasm (#3)
    * Cookies + Param services added
    
    * form added
    
    * index controller added
    
    * Services added
    
    * UserService added
    
    * ProductController added
    
    
    Co-authored-by: Xuan Hao <nxhao3003@gmail.com>

---
+++ src/main/java/com/fpoly/java5/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.fpoly.java5.controller;
+
+
+import com.fpoly.java5.entity.Category;
+import com.fpoly.java5.entity.Product;
+import com.fpoly.java5.service.CategoryService;
+import com.fpoly.java5.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+    @Autowired
+    CategoryService categoryService;
+
+    // localhost:8080/product/{categoryID}
+    @GetMapping("/{categoryID}")
+    public String doGetProductbyCgrId(@PathVariable("categoryID") String categoryId, Model model) {
+        List<Product> productList = productService.findByCategoryId(categoryId);
+        model.addAttribute("productList", productList);
+        List<Category> categoryList = categoryService.findAll();
+        model.addAttribute("categoryList",categoryList);
+
+        return "detailProduct";
+    }
+
+    // localhost:8080/product?productID=???
+    @GetMapping("")
+    public String doGetProductById(@RequestParam("productID") String productId, Model model) {
+        Product product = productService.findByProductId(productId);
+        model.addAttribute("product", product);
+        List<Category> categoryList = categoryService.findAll();
+        model.addAttribute("categoryList", categoryList);
+        return "detailProduct";
+    }
+
+}
---
+++ src/main/java/com/fpoly/java5/service/CartService.java
@@ -0,0 +1,9 @@
+package com.fpoly.java5.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+
+}
