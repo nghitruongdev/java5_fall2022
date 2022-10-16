ac283eafa2fbfd5b456f99e28b4c0fc71168224a
Vũ Quốc Thái An <thaian100502@gmail.com>
2022-10-16 12:43:08 +0700

    Them product helper

--- src/main/java/com/fpoly/java5/controller/adminController.java
+++ src/main/java/com/fpoly/java5/controller/adminController.java
@@ -1,13 +1,12 @@
 package com.fpoly.java5.controller;
 
 import com.fpoly.java5.entity.Product;
-import com.fpoly.java5.helper.imgHelper;
+import com.fpoly.java5.helper.ProductHelper;
 import com.fpoly.java5.repo.CategoryRepository;
 import com.fpoly.java5.repo.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,17 +27,7 @@ public class adminController {
     @Autowired
     ProductRepository productDAO;
 
-    imgHelper imgHelper = new imgHelper();
+    ProductHelper productHelper = new ProductHelper();
 
-    @PostMapping("index/insert")
-    public String Inser(Model model, @RequestParam("name") String nameString, @RequestParam("price") Optional<String> priceString, @RequestParam ("description") String descriptionString, @RequestParam("quantity") Optional<String> quantityString, @RequestParam("category_id") String categoryIDString, HttpServletRequest req) throws IOException, ServletException {
-        String tenHinhAnh =  imgHelper.uploadImage(req).equals("")?req.getParameter("hinhAnhTonTai"):imgHelper.uploadImage(req);
-        List<Optional<String>> list = new ArrayList<>();
-//        list.add(categoryIDString);
-//        list.add(priceString);
 
-        Product product = new Product();
-        productDAO.save(product);
-        return "/admin/insert";
-    }
 }
